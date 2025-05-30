54e41d63888b68bfe09bf4dc7a9adb41c31572cf
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2025-05-30 21:13:39 +0400

    Add workshop 1 code

---
+++ workshop_1/assignment.py
@@ -0,0 +1,10 @@
+money_in_wallet = 100
+sandwich_price = 10
+
+print("Wallet:", money_in_wallet)
+print("Sandwich:", sandwich_price)
+
+# money_in_wallet = money_in_wallet - sandwich_price
+money_in_wallet -= sandwich_price
+# money_in_wallet - sandwich_price
+print("Wallet:", money_in_wallet)
---
+++ workshop_1/main.py
@@ -0,0 +1,28 @@
+print("Hello world!")
+
+
+print("hello there".upper())
+print("HELLO There".lower())
+print(5 + 9 * (1 + 9))
+# print(type(7 * 5))
+print(7 / 2, type(7 / 2))
+# print(7 / 2)
+# print(type(7 / 2))
+print("hi" + " " + "There")
+
+# print(type("6" + "7"))
+
+print(5 == 5)
+print(5 == 6)
+print("hi" == "Goodbye")
+print("six" == 6)
+print("6" == 6)
+print("6" != 6)
+print(5 > 7)
+print(5 < 7)
+print(5 <= 7)
+print(5 >= 5)
+print("hi" > "bye")
+print("H" > "h")
+
+
