ec88100d95f28a74d3e4531c08ed031d315714f8
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2025-07-11 21:16:30 +0400

    Add workshop 11 code

---
+++ django_project_1/polls/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Question(models.Model):
+    text = models.CharField(max_length=255)
+
+    publication_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    votes = models.PositiveIntegerField(default=0)
+    
+    def __str__(self):
+        return self.text
